0f3a158bb70b1a6ba8df9f7b3693e9288f16c86b
comradevinh <vinhbizwiznguyen@gmail.com>
2018-05-23 22:54:45 -0400

    updated stuffs for the fse

--- src/application/Attack.java
+++ src/application/Attack.java
@@ -1,7 +1,7 @@
 package application;
 
 //Vinh Nguyen and Maggie Maun
-//Our FSE which will be a role-playing game with a developed story
+//Our FSE which will be a role-playing game with a developed story; Massey Quest
 
 
 import javafx.application.Application; //inputting all necessary aspects
@@ -26,6 +26,7 @@ public class Attack implements Serializable {
 
     ArrayList<Character> characters = new ArrayList<Character>();
     ArrayList<Weapons> weapons= new ArrayList<Weapons>();
+    ArrayList<Character> enemies = new ArrayList<Character>();
 
     Weapons weaponsArray[]= new Weapons[5]; //will serve as temprary array when putting weapons into treeset
 
@@ -33,7 +34,7 @@ public class Attack implements Serializable {
 		
 	}
 
-	public void save(){
+	public void save() {
 
         try { //serializing occurs, will act as saving data, very efficient
             FileOutputStream saveState = new FileOutputStream("/tmp/savestate.ser");
@@ -47,10 +48,6 @@ public class Attack implements Serializable {
         } catch (IOException i) {
             i.printStackTrace();
         }
-
-    }
-
-    public void damage(Character one, Character two){ //will be the method used to calculate damage
 
     }
 
@@ -109,7 +106,19 @@ public class Attack implements Serializable {
 			System.out.println("Datafile not found");
 		}
 
-        weaponsArray[0]=weapons.get(0);
+        try{
+            Scanner inFile3 = new Scanner(new File("Enemy Statistics.txt")); //Scanner as input; input is in file
+            while (inFile3.hasNext()) {
+                enemies.add(new Character(inFile3.nextLine()));
+            }
+            //inputing all the characters, which will then be put into the treeset
+            inFile3.close();
+        }
+        catch(IOException e) {
+            System.out.println("Datafile not found");
+        }
+
+        weaponsArray[0]=weapons.get(0); //inputing character weapons into array, putting into treeset
         profileMap.put(characters.get(0),weaponsArray);
         weaponsArray[0]=weapons.get(4);
         profileMap.put(characters.get(1),weaponsArray);
@@ -120,11 +129,126 @@ public class Attack implements Serializable {
         weaponsArray[0]=weapons.get(16);
         profileMap.put(characters.get(4),weaponsArray);
 
+        weaponsArray[0]=weapons.get(1); //inputing enemy weapons into array, putting into treeset
+        enemyProfileMap.put(enemies.get(0),weaponsArray);
+        weaponsArray[0]=weapons.get(18);
+        enemyProfileMap.put(enemies.get(1),weaponsArray);
+        weaponsArray[0]=weapons.get(19);
+        enemyProfileMap.put(enemies.get(2),weaponsArray);
+        weaponsArray[0]=weapons.get(19);
+        enemyProfileMap.put(enemies.get(3),weaponsArray);
+        weaponsArray[0]=weapons.get(20);
+        enemyProfileMap.put(enemies.get(4),weaponsArray);
+        weaponsArray[0]=weapons.get(17);
+        enemyProfileMap.put(enemies.get(5),weaponsArray);
+        weaponsArray[0]=weapons.get(27);
+        enemyProfileMap.put(enemies.get(6),weaponsArray);
+
         save();
 
         //inputing all the data into the treeset for preparing the beginning of the game
 
 	}
+
+	//ATTACK CALCULATIONS
+
+    public int damage(Character cOne, Character cTwo, Weapons wOne, Weapons wTwo){ //will be the method used to calculate damage
+
+        int attackPower=cOne.getStrength()+wOne.getMight(); //will be the attack power of the individual
+
+        if (wOne.getName().equals("Sword")){ //altering attack power, depnding on weapon triangle
+            if (wTwo.getName().equals("Lance")){
+                attackPower-=1;
+            }
+            else if(wTwo.getName().equals("Axe")) {
+                attackPower+=1;
+            }
+        }
+
+        else if (wOne.getName().equals("Lance")){ //altering attack power, depnding on weapon triangle
+            if (wTwo.getName().equals("Axe")){
+                attackPower-=1;
+            }
+            else if(wTwo.getName().equals("Sword")) {
+                attackPower+=1;
+            }
+        }
+
+        else if (wOne.getName().equals("Axe")){ //altering attack power, depnding on weapon triangle
+            if (wTwo.getName().equals("Sword")){
+                attackPower-=1;
+            }
+            else if(wTwo.getName().equals("Lance")) {
+                attackPower+=1;
+            }
+        }
+
+        //checks for critical hit
+
+        boolean critActivate=false; //will turn true if crit is activated, and will do twice the damage
+
+        for (int i=0; i<cOne.getSkills().size();i++){
+            if(cOne.getSkills().get(i).equals("Critical")){ //checks if there is the critical skill
+                Random critChance= new Random();
+                int critical=critChance.nextInt(8);
+                if (critical==1) critActivate = true; //multiplies damage by 2, when critical occurs
+            }
+        }
+
+        //will reduce , if shield skill is present and activated
+
+        for (int i=0; i<cTwo.getSkills().size();i++){
+            if(cOne.getSkills().get(i).equals("Critical")){ //checks if there is the critical skill
+                Random shieldChance= new Random();
+                int shield=shieldChance.nextInt(8);
+                if (shield==1) attackPower -= 10; //multiplies damage by 2, when critical occurs
+            }
+        }
+
+        //actual calculations of damage; attack power subtracted by defence
+
+        if (cOne.getType().equals("")){ //attack power subtracts by magical defence
+            if (critActivate==true) attackPower= (attackPower-cTwo.getMDefence())*2;
+            else attackPower -=cTwo.getMDefence();
+        }
+        else{ //attack power subtracts by physical defence
+            if (critActivate==true) attackPower= (attackPower-cTwo.getDefence())*2;
+            else attackPower -= cTwo.getDefence();
+        }
+
+
+        //checks if damage dealt is less than 0
+
+        if (attackPower<=0){
+            return 0; //prevents characters from dealing negative damage
+        }
+        else return attackPower; //returns the damage done, which will be inputed into different methods
+
+    }
+
+    //Accuracy calculations; determines whether character will hit or not
+
+    public boolean hitMiss(Character cOne, Character cTwo, Weapons wOne, Weapons wTwo){
+	    int slowDown=cTwo.getBuild()-wTwo.getWeight();
+	    if (slowDown>=0) slowDown=0;
+        //determining the hit
+        int hitRate= (wOne.getHit()+(cOne.getSkill()*2))-(slowDown*3);
+
+        if (hitRate>100){
+            hitRate=100; //ensures hit doesn't go over limit
+        }
+        if (hitRate<0){
+            hitRate=-1; //ensures hit doesn't go under the limit
+        }
+
+        Random hitChance= new Random();
+        int hit=hitChance.nextInt(100);
+        if (hit<=hitRate){ //if it falls within range they will hit
+            return true;
+        }
+        else return false; //if it doesn't fall within range they will not hit
+
+    }
 
 }
 
@@ -148,6 +272,8 @@ class Character{
 	
 	private int WPNTYPES; //number that determines how many types of weapons can be used
 	private int SKLNUM; //number of skills a character has
+	
+	private String Type; //determines whether they're magical or physical fighters
 	
 	private int EXP; //experience that will determine level ups
 	
@@ -185,6 +311,7 @@ class Character{
 		}
 		
 		this.EXP=Integer.parseInt(data[tempPos+SKLNUM+1]);
+		this.Type=data[tempPos+SKLNUM+2];
 		
 		HP=HPMAX; //assigns the original health of a character to the max health
 		
@@ -232,9 +359,17 @@ class Character{
 		return BLD;
 	}
 	
+	public String getType() {
+		return Type;
+	}
+	
 	public int getExp() {
 		return EXP;
 	}
+
+    public ArrayList<String> getSkills(){
+        return Skills;
+    }
 	
 	//METHODS TO ALTER INFORMATIONS
 	
@@ -272,6 +407,7 @@ class Weapons{
 	private int Weight; //the weight of the weapon, when subtracted from build, if a negative number comes out, it will decrease speed
 	
 	private int STR; //will be used to determine might of weapon
+    private int HIT; //hit rate of the weapon, assists in accuracy
 	
 	public Weapons(String weaponStat) { //constructor for weapon class
 		
@@ -283,6 +419,7 @@ class Weapons{
 		this.Value=Integer.parseInt(data[3]);
 		this.Weight=Integer.parseInt(data[4]);
 		this.STR=Integer.parseInt(data[5]);
+		this.HIT=Integer.parseInt(data[6]);
 		
 	}
 	
@@ -311,6 +448,8 @@ class Weapons{
 	public int getMight() {
 		return STR;
 	}
+
+	public int getHit() {return HIT;}
 	
 	//METHODS TO ALTER INFORMATIONS
 	
